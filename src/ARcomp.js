ff31143c8b74875352b9c0c6ad44f2edba0f1612
Jobin Babu Ayathil <jobinb6444@outlook.com>
2022-08-17 11:49:48 +0530

    Update ARcomp.js

--- src/ARcomp.js
+++ src/ARcomp.js
@@ -16,16 +16,13 @@ export default class ARcomp extends React.Component {
         style={{ width: "800px", height: "800px" }}
         ref={mount => { this.mount = mount; }}
       >
-        <Scene>
-        <a-scene
+    <a-scene
         vr-mode-ui="enabled: false"
-		    embedded
+		embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
-        
-        <a-camera gps-camera rotation-reader alert="true"></a-camera>
-        <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="1.5 1.5 1.5" gps-entity-place="longitude: 73.1339639; latitude: 19.0025326;" animation-mixer/>
-        </a-scene>
-        </Scene>
+        <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: 73.133662; latitude: 19.002997;" animation-mixer/>
+            <a-camera gps-camera rotation-reader></a-camera>
+	</a-scene>
       </div>
     );
   }
